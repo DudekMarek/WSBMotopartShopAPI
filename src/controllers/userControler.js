83848b2b99df5e4d0fb41390ab04d11cadc1cce4
DudekMarek <dudek371@gmail.com>
2023-11-04 13:32:01 +0100

    add delete and update

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -6,8 +6,8 @@ async function get(req, res, next) {
       res.json(users);
     })
     .catch((err) => {
-      console.error("Error fetching users:", error);
-      res.status(500).json({ error: "Failed to fetch users" });
+      console.error(`Error fetching users: ${err}`);
+      res.status(500).json({ error: err });
     });
 }
 
@@ -25,8 +25,49 @@ async function create(req, res, next) {
       });
     })
     .catch((err) => {
+      console.error(`Error while updating creating: ${err}`);
       res.json({ error: err });
     });
 }
 
-export { get, create };
+async function remove(req, res, next) {
+  const userId = req.params.id;
+  User.findByPk(userId)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ error: "User not found" });
+      } else {
+        return user.destroy();
+      }
+    })
+    .then(() => {
+      res.status(200).json({ message: `User with Id: ${userId} deleted` });
+    })
+    .catch((err) => {
+      console.error(`Error while deleting user: ${err}`);
+      res.status(500).json({ error: err });
+    });
+}
+
+async function update(req, res, next) {
+  const userId = req.params.id;
+  const updatedUser = req.body;
+
+  User.findByPk(userId)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ error: "User not found" });
+      } else {
+        return user.update(updatedUser);
+      }
+    })
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(`Error while updating user: ${err}`);
+      res.status(500).json({ error: err });
+    });
+}
+
+export { get, create, remove, update };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,15 @@
 import express from "express";
 
-import * as userControler from '../controllers/userControler.js'
+import * as userControler from "../controllers/userControler.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.get('/', userControler.get)
+userRouter.get("/", userControler.get);
 
-userRouter.post('/', userControler.create)
+userRouter.post("/", userControler.create);
+
+userRouter.delete("/:id", userControler.remove);
+
+userRouter.patch("/:id", userControler.update);
 
 export default userRouter;
