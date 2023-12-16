046c1147ad898e8065f1744c9717a851527a52ad
unknown <aloneviahome@gmail.com>
2023-12-16 06:00:00 +0100

    productController update

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import Product from "../models/productModel";
 import {CreateProduct, UpdateProduct } from "../schemas/productSchema"
 import { plainToInstance } from "class-transformer"
-import { validateInstance, EntityNotFoundError, handleError } from "../helpers/validation";
-
+import { getEntityById } from '../services/relationService'
+import { validateInstance, handleError } from "../helpers/validation";
 
 async function get(req: Request, res: Response) {
   try {
@@ -20,7 +20,7 @@ async function create(req: Request, res: Response) {
     const product = plainToInstance(CreateProduct, req.body);
     await validateInstance(product);
     const createdProduct = await Product.create({ ...product });
-    res.send(201).send(createdProduct);
+    res.status(201).send(createdProduct);
   } catch(err) {
     handleError(err, res);
   }
@@ -28,11 +28,8 @@ async function create(req: Request, res: Response) {
 
 async function update(req:Request, res: Response) {
   try {
-    const productId = req.params.id;
-    const existingProduct = await Product.findByPk(productId);
-    if(!existingProduct) {
-      throw new EntityNotFoundError("Product not found");
-    }
+    const productId = parseInt(req.params.id, 10);
+    const existingProduct = await getEntityById(Product, productId)
     const product = plainToInstance(UpdateProduct, req.body);
     await validateInstance(product);
     const updatedProduct = await existingProduct.update(product);
@@ -44,11 +41,8 @@ async function update(req:Request, res: Response) {
 
 async function remove(req:Request, res: Response) {
   try {
-    const productId = req.params.id;
-    const existingProduct = await Product.findByPk(productId);
-    if(!existingProduct) {
-      throw new EntityNotFoundError;
-    }
+    const productId = parseInt(req.params.id, 10);
+    const existingProduct = await getEntityById(Product, productId);
     await existingProduct.destroy();
     res.status(200).json({ message: `Product with ${productId} deleted` });
   } catch(err) {
